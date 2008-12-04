5230ed1121a101a0ff7490272dd2d804996003ef
Denis-de-Bernardy <Denis-de-Bernardy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-12-04 12:30:17 +0000

    frame buster v.4.0
    git-svn-id: https://plugins.svn.wordpress.org/sem-frame-buster/trunk@77438 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,38 @@
+=== Frame Buster ===
+Contributors: Denis-de-Bernardy, Semiologic
+Donate link: http://buy.semiologic.com/drink
+Tags: frames, frame-buster
+Requires at least: 2.0
+Tested up to: 2.6.5
+Stable tag: 4.0
+
+The frame buster plugin for WordPress will thwart any attempt to load your site in a frame.
+
+
+== Description ==
+
+The frame buster plugin for WordPress will thwart any attempt to load your site in a frame.
+
+If you've ever seen your site trapped within a frame surrounded by ads, you certainly know why such a plugin can be useful.
+
+
+== Installation ==
+
+1. Upload the plugin folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+
+== Frequently Asked Questions ==
+
+= It Does Not Work At All =
+
+Make sure your theme has the following in between its `<head>` and `</head>` tags:
+
+    <?php wp_head(); ?>
+
+
+= Help Me! =
+
+Feel very free to report issues in the [Semiologic forums](http://forum.semiologic.com).
+
+Please note, however, that we generally stick to assisting [Semiologic Pro](http://www.getsemiologic.com) users.
---
+++ sem-frame-buster.php
@@ -0,0 +1,72 @@
+<?php
+/*
+Plugin Name: Frame Buster
+Plugin URI: http://www.semiologic.com/software/wp-tweaks/frame-buster/
+Description: Prevents your blog from being loaded into a frame.
+Author: Denis de Bernardy
+Version: 4.0
+Author URI: http://www.semiologic.com
+*/
+
+/*
+Terms of use
+------------
+
+This software is copyright Mesoconcepts (http://www.mesoconcepts.com), and is distributed under the terms of the GPL license, v.2.
+
+http://www.opensource.org/licenses/gpl-2.0.php
+**/
+
+
+class sem_frame_buster
+{
+	#
+	# Constructor
+	#
+
+	function init()
+	{
+		add_action('wp_footer', array('sem_frame_buster', 'kill_frame'));
+	} # init()
+
+
+	#
+	# kill_frame()
+	#
+
+	function kill_frame()
+	{
+		if ( !is_preview() )
+		{
+			$home_url = strtolower(get_option('home'));
+
+			echo <<<KILL_FRAME_SCRIPT
+<script type="text/javascript">
+<!--
+try
+{
+	var parent_location = new String(parent.location);
+	var top_location = new String(top.location);
+	var cur_location = new String(document.location);
+	parent_location = parent_location.toLowerCase();
+	top_location = top_location.toLowerCase();
+	cur_location = cur_location.toLowerCase();
+
+	if ( ( top_location != cur_location ) && parent_location.indexOf('{$home_url}') != 0 )
+	{
+		top.location.href = document.location.href;
+	}
+}
+catch ( err )
+{
+	top.location.href = document.location.href;
+}
+//-->
+</script>
+KILL_FRAME_SCRIPT;
+		}
+	} # kill_frame()
+} # sem_frame_buster
+
+sem_frame_buster::init();
+?>
